b4a23845249851b4f2b738fabd596884652597bf
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-07 19:44:16 -0500

    minor changes

--- mergesort.js
+++ mergesort.js
@@ -1,3 +1,4 @@
+// split an array down the middle into two halves
 function split(arr) {
   let splitIndex = Math.floor(arr.length / 2);
   let firstHalf = arr.slice(0, splitIndex);
@@ -5,11 +6,15 @@ function split(arr) {
   return [firstHalf, secondHalf];
 }
 
+// merge two sorted arrays into one sorted array
 function merge(leftHalf, rightHalf) {
   let result = [];
   let leftIndex = 0;
   let rightIndex = 0;
 
+  // push the lesser of the first element of the left half and
+  // the first element of the right half into a new array
+  // as long as both arrays still contain something
   while (leftIndex < leftHalf.length && rightIndex < rightHalf.length) {
     if (leftHalf[leftIndex] < rightHalf[rightIndex]) {
       result.push(leftHalf[leftIndex++]);
@@ -18,6 +23,8 @@ function merge(leftHalf, rightHalf) {
     }
   }
 
+  // when one array is empty, concatenate the remainder of the other
+  // array directly onto the result
   if (leftIndex === leftHalf.length) {
     while (rightIndex < rightHalf.length) {
       result.push(rightHalf[rightIndex++]);
@@ -31,6 +38,8 @@ function merge(leftHalf, rightHalf) {
   return result;
 }
 
+// split arrays until each element is in its own array (aka sorted)
+// then merge them by sorting from the first element of each
 function mergeSort(arr) {
   if (arr.length === 1) {
     return arr;
